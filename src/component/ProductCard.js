11c292c03afec65270c330fe845dd1129d9f5013
Yonghyun Kim <81367886+yonghyun421@users.noreply.github.com>
2024-03-27 18:33:32 +0900

    hnm practice

---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img alt="product" src="https://noona-hnm.netlify.app/ankle-jeans.jpeg" />
+      <div>Conscious Choice</div>
+      <div>벨티드 트윌 코트</div>
+      <div>₩ 90000</div>
+      <div>신제품</div>
+    </div>
+  );
+};
+
+export default ProductCard;
