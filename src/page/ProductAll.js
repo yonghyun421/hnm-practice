f97bddde823a0b512c73edc82a5107e058b5d6ab
Yonghyun Kim <81367886+yonghyun421@users.noreply.github.com>
2024-03-27 18:36:37 +0900

    lint check

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import ProductCard from "../component/ProductCard";
 
 const ProductAll = () => {
-  const [productList, setProductList] = useState([]);
+  //   const [productList, setProductList] = useState([]);
   const getProducts = async () => {
     let url = "http://localhost:5000/products";
     let response = await fetch(url);
     let data = await response.json();
-    setProductList(data);
+    // setProductList(data);
   };
 
   useEffect(() => {
