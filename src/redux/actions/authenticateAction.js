fb036c7b9b8cda4b2a91f8b3bfa0a8d401356b81
Yonghyun Kim <81367886+yonghyun421@users.noreply.github.com>
2024-04-03 16:09:54 +0900

    redux-toolkit 적용

--- src/redux/actions/authenticateAction.js
+++
@@ -1,7 +0,0 @@
-function login(id, password) {
-  return (dispatch, getState) => {
-    dispatch({ type: "LOGIN_SUCCESS", payload: { id, password } });
-  };
-}
-
-export const authenticateAction = { login };
--- src/route/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ProductDetail from "../page/ProductDetail";
-import { Navigate } from "react-router-dom";
-
-const PrivateRoute = ({ authentication }) => {
-  return authentication ? <ProductDetail /> : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
